f58bf40dbd950fff533a92dbfc7b594ba7abd43a
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-09 19:01:39 -0500

    Added comments

--- script/script.js
+++ script/script.js
@@ -57,9 +57,16 @@ function displayScores(response){
             teamArray.push(team);
         }     
     }
+    // Now that everyone's points have been added
+    // sort the array based on the total score
     teamArray.sort(scoreSort);
+    // Now that we have all the data nicely sorted into teams
+    // We can display it on the screen!
     buildTeamPyramid();
 }
+// We want a pyramid, where the top level, with the current winner, is the
+// only column in the row. Then the next level has two columns (with two houses)
+// and the three, and so on.
 function buildTeamPyramid(){
     var level=0;
     var cols=0;
@@ -69,13 +76,21 @@ function buildTeamPyramid(){
         console.log("Team "+team.teamName+", level:"+level);
         var teamIcon=$("<div>").attr("id",team.teamName);
         teamIMG=$("<img>").attr("src",assetPath+team.icon);
+
+        // In addition, we make the current winner 1.75 times larger, and scale the others
+        // based on what percentage of points they have compared to the current winner.
         var teamWidth=baseImageWidth+((baseImageWidth*1.75)*(team.score/highScore))+"px";
         teamIMG.css("width",teamWidth);
         teamIcon.append(teamIMG);
+
+        // And of course we print the team's score
         scoreDisplay=$("<span>");
         scoreDisplay.addClass("scoreBG");
         scoreDisplay.append($("<h3>").text(team.score));
         teamIcon.append(scoreDisplay);
+
+        // Now that we've assembled all the data, we have to figure out where
+        // to put it. If it's a new tier, then we make a new row and start adding to it
         if(cols>=level){
             cols=0;
             level++;
@@ -83,20 +98,27 @@ function buildTeamPyramid(){
             tier.append($("<div>").addClass("col").append(teamIcon));
             pyramid.append(tier);
         }
+        // If not, then we can just keep adding to the tier we have
         else 
             tier.append($("<div>").addClass("col").append(teamIcon));    
         cols++;
     }
+    // Then we add all the tiers to the pyramid 
     pyramid.append(tier);
+    // And add the pyrmid to the document
     $("body").append(pyramid);
 
+    // The first element in teamArray is the current currentWinner. Let's make a huge
+    // version of their icon as a backdrop!
     var winningIcon=assetPath+teamArray[0].icon;
     var backdrop=`<div class="container-fluid" id="backdrop" style="background-image: url('${winningIcon}');"></div>`;
     $("body").append(backdrop);
 }
 
 
-
+// scoreSort is sent two scores, and returns a positive number
+// is the second one is bigger, and a negative number if the
+// first one is bigger
 function scoreSort(a, b){
     return b.score-a.score;
 }
