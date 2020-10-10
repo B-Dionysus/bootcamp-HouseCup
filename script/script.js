a3e1e347f9572bdf0fbff5c0fadb876defe1cb32
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-09 19:11:52 -0500

    Almost forgot! Had to switch to live data!!

--- script/script.js
+++ script/script.js
@@ -7,8 +7,7 @@ var baseImageWidth=70;
 
 $("document").ready(init);
 function init(){
-    // getScores();
-    displayScores(JSON.parse(`[{"id":1,"studentName":"Adrian Mustafa","teamName":"aol","pointsEarned":0},{"id":2,"studentName":"Aida Blinstrubyte","teamName":"aol","pointsEarned":0},{"id":3,"studentName":"Daniel Viramontes","teamName":"aol","pointsEarned":0},{"id":4,"studentName":"Ben Resnicoff","teamName":"aol","pointsEarned":0},{"id":5,"studentName":"Steve Kavuu","teamName":"aol","pointsEarned":0},{"id":6,"studentName":"Jake Wagner","teamName":"aol","pointsEarned":1350},{"id":7,"studentName":"Matt Hiatt","teamName":"napster","pointsEarned":0},{"id":8,"studentName":"Justin Albert","teamName":"napster","pointsEarned":500},{"id":9,"studentName":"Flory Ann Evia","teamName":"napster","pointsEarned":0},{"id":10,"studentName":"Erik Tomlinson","teamName":"napster","pointsEarned":0},{"id":11,"studentName":"Toni Rose Debelen","teamName":"napster","pointsEarned":0},{"id":12,"studentName":"Alex Eesley","teamName":"napster","pointsEarned":0},{"id":13,"studentName":"Zach Smelcer","teamName":"netscape","pointsEarned":0},{"id":14,"studentName":"Kevin Miller","teamName":"netscape","pointsEarned":0},{"id":15,"studentName":"Trey Jahner","teamName":"netscape","pointsEarned":0},{"id":16,"studentName":"Elizabeth Reuter","teamName":"netscape","pointsEarned":0},{"id":17,"studentName":"Aaron Ray","teamName":"netscape","pointsEarned":0},{"id":18,"studentName":"Justin Yocus","teamName":"netscape","pointsEarned":0},{"id":19,"studentName":"Ryan Wills","teamName":"winamp","pointsEarned":0},{"id":20,"studentName":"John Cushing","teamName":"winamp","pointsEarned":0},{"id":21,"studentName":"Katie Reid-Anderson","teamName":"winamp","pointsEarned":0},{"id":22,"studentName":"Michael Chang","teamName":"winamp","pointsEarned":0},{"id":23,"studentName":"Benjamin Dionysus","teamName":"winamp","pointsEarned":1150},{"id":24,"studentName":"Olivia Park","teamName":"winamp","pointsEarned":0},{"id":25,"studentName":"Agnes Chueng","teamName":"xanga","pointsEarned":1000},{"id":26,"studentName":"Max Jeanty","teamName":"xanga","pointsEarned":0},{"id":27,"studentName":"Sarah Barkley","teamName":"xanga","pointsEarned":0},{"id":28,"studentName":"Sam Fullerton","teamName":"xanga","pointsEarned":0},{"id":29,"studentName":"Chris Kalama","teamName":"xanga","pointsEarned":0},{"id":30,"studentName":"Owen Roth","teamName":"xanga","pointsEarned":0}]`))
+     getScores();
 }
 function getScores(){
     const corsAnywhere = `https://cors-anywhere.herokuapp.com/`;
@@ -73,7 +72,6 @@ function buildTeamPyramid(){
     var pyramid=$("<div>").addClass("container text-center");
     var tier;
     for(team of teamArray){
-        console.log("Team "+team.teamName+", level:"+level);
         var teamIcon=$("<div>").attr("id",team.teamName);
         teamIMG=$("<img>").attr("src",assetPath+team.icon);
 
